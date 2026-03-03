0f89e0159e9a52e937a38b171692c33da4cf692e
draftworks-repo <draftworks000@gmail.com>
2026-03-03 12:26:04 +0530

    gallery page

---
+++ app/gallery/page.tsx
@@ -0,0 +1,15 @@
+import { Header } from "@/components/Header";
+import { Gallery } from "@/components/Gallery";
+import { CTA } from "@/components/CTA";
+import { Footer } from "@/components/Footer";
+
+export default function GalleryPage() {
+  return (
+    <main>
+      <Header />
+      <Gallery />
+      <CTA />
+      <Footer />
+    </main>
+  );
+}
